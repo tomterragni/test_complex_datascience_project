73194c812a601dd3fb025f5009566e73537eb9f4
Tom Terragni <tomterragni@gmail.com>
2019-05-19 16:33:28 +0200

    first commit with cookiecutter structure and DVC init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A test Data Science project including code and data versioning, model development, serialisation and serving, as well as dashboards, and notebooks.',
+    author='Tom Terragni',
+    license='',
+)
